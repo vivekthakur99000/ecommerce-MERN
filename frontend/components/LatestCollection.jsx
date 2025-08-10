1369af1749fffcdc8c488ee0edf17b49a53b22d8
vivek thakur <vivekthakur99000@gmail.com>
2025-08-10 16:16:34 +0530

    Enhances navigation and product filtering features
    Implements a responsive navigation bar with a dropdown menu for smaller screens, improving user experience.
    
    Adds product filtering functionality, allowing users to filter by category and subcategory, and sort products by price.
    
    Integrates context for managing shop data, enhancing state management across components.
    
    Introduces additional components on the homepage for a more comprehensive layout.
    
    Fixes layout issues and optimizes CSS for better performance and readability.

---
+++ frontend/components/LatestCollection.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { products } from '../src/assets/assets';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+
+const LatestCollection = () => {
+
+    const {Products} = useContext(ShopContext)
+
+    // console.log(products);
+
+    const [latestProduct, setLatestProduct] = useState([])
+
+    useEffect(() => {
+        setLatestProduct(products.slice(0, 10))
+    }, [])
+    
+
+  return (
+    <div className='my-10'>
+      <div className="text-center py-8 text-3xl">
+        <Title text1={'Latest'} text2={'Collection'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sunt vel est esse rerum eum, quas dolorum? Voluptatum, cumque veniam!
+        </p>
+      </div>
+
+      {/* rendering products */}
+
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ">
+        {
+            latestProduct.map((product, index) => (
+                <ProductItem key={index} id={product._id} image={product.image} name={product.name} price={product.price}  />
+            ))
+        }
+      </div>
+
+      
+
+    </div>
+  )
+}
+
+export default LatestCollection
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "../context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
-  
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
