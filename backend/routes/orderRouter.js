83d7a79a2468e4b68e8ca28151c46c147fab3446
vivek thakur <vivekthakur99000@gmail.com>
2025-08-18 19:36:11 +0530

    feat: Add order router with admin and user order functionalities

---
+++ backend/routes/orderRouter.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderRazorPay,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+
+import adminAuth from "../middleware/adminAuth.js"
+import authUser from "../middleware/auth.js"
+
+const orderRouter = express.Router();
+
+// admin features
+orderRouter.post("/list", adminAuth,allOrders)
+orderRouter.post("/status", adminAuth,updateStatus)
+
+// payment
+orderRouter.post("/place", authUser,placeOrder)
+orderRouter.post("/stripe", authUser,placeOrderStripe)
+orderRouter.post("/razorpay", authUser,placeOrderRazorPay)
+
+// user feature
+
+orderRouter.post("/usersorders", authUser, userOrders)
+
+
+export default orderRouter;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRouter.js";
 
 // App config
 const app = express();
@@ -21,6 +22,7 @@ app.use(cors());
 app.use("/api/user", userRouter)
 app.use("/api/product", productRoute)
 app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 
 app.get("/", (req, res) => {
